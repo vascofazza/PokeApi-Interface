daa610950a0e04254cbd8eed7c6656c4a5591cea
Federico Scozzafava <federico.scozzafava@gmail.com>
2018-12-20 17:26:09 +0100

    prima versione funzionante

--- main.py
+++ main.py
@@ -1,18 +1,73 @@
+import io
+from io import StringIO
 from tkinter import *
+
+import PIL
+from PIL import ImageTk
+
 import pokemonapi as pk
 
+api = pk.PokemonApi()
 
-def get_pokemon():
-    response = requests.get('https://pokeapi.co/api/v2/pokemon-form/151/')
-    if response.status_code == 200:
-        return json.loads(response.content.decode('utf-8'))
-    else:
-        return None
+window = Tk()
 
+window.title("PokeApi Interface")
 
-print(get_pokemon())
-sys.exit(0)
+txt = Entry(window, width=10)
 
-main_window = Tk()
+txt.grid(column=0, row=0)
 
-main_window.mainloop()
+name_label = Label(window, text="Nome: ")
+
+name_label.grid(column=0, row=1)
+
+tipo_label = Label(window, text="Tipo: ")
+
+tipo_label.grid(column=0, row=2)
+
+front_sprite_label = Label(window)
+
+front_sprite_label.grid(column=1, row=1)
+
+back_sprite_label = Label(window)
+
+back_sprite_label.grid(column=1, row=2)
+
+front_sprite_s_label = Label(window)
+
+front_sprite_s_label.grid(column=2, row=1)
+
+back_sprite_s_label = Label(window)
+
+back_sprite_s_label.grid(column=2, row=2)
+
+def load_sprite(name, sprite):
+    label = None
+    if 'front' in name:
+        if 'shiny' in name:
+            label = front_sprite_s_label
+        else:
+            label = front_sprite_label
+    elif 'back' in name:
+        if 'shiny' in name:
+            label = back_sprite_s_label
+        else:
+            label = back_sprite_label
+    bytes = io.BytesIO(sprite)
+    image = PIL.ImageTk.Image.open(bytes)
+    img = PIL.ImageTk.PhotoImage(image)
+    label.configure(image=img)
+    label.image = img
+
+def clicked():
+    pokemon = api.get_pokemon(poke_name=txt.get())
+    name_label.configure(text='Nome: ' + pokemon.name)
+    tipo_label.configure(text='Tipo: ' + pokemon.get_tipo())
+    for name, sprite in pokemon.get_sprites().items():
+        load_sprite(name, sprite)
+
+btn = Button(window, text="Ottieni", command=clicked)
+
+btn.grid(column=1, row=0)
+
+window.mainloop()
